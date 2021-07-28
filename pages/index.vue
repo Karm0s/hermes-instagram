c103626f07385c3f125070d588a65cf8ac4d17c2
Karm0s <bouchenna.yanis@protonmail.com>
2021-07-28 15:45:51 +0200

    Initial commit
    Install vuesax, nuxt-property-decorator, fontawesome, general-sans font

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app"> 
+    <div>This is the homepage</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.app {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'GeneralSans-Bold';
+}
+</style>
---
+++ plugins/vuesax.ts
@@ -0,0 +1,5 @@
+import {Vue} from 'nuxt-property-decorator'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+
+Vue.use(Vuesax)
