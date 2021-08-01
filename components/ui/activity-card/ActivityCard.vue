b428db24468cc9c11189f8da9c8a157cab1f4f7a
Yanis <bouchenna.yanis@protonmail.com>
2021-08-02 00:59:28 +0200

    Add padding to Activity card in Dashboard

--- components/ui/activity-card/ActivityCard.vue
+++ components/ui/activity-card/ActivityCard.vue
@@ -1,7 +1,7 @@
 <template>
     <app-card>
         <h1 class="card-title stat-text text-lg lg:text-2xl generalsans-medium text-center mt-4 mb-8">{{this.title}}</h1>
-        <div class="flex flex-col w-full justify-start divide-y-2 divide-gray-500 divide-dashed">
+        <div class="flex flex-col w-full justify-start divide-y-2 divide-gray-500 divide-dashed px-4 md:px-8">
             <slot/>
         </div>
     </app-card>
