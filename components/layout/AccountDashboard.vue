2da55cbde5fe54bf597ceee3e4e1918d11aa1603
Yanis <bouchenna.yanis@protonmail.com>
2021-08-02 00:52:46 +0200

    Update Account Settings forms to be responsive

--- components/layout/AccountDashboard.vue
+++ components/layout/AccountDashboard.vue
@@ -3,17 +3,17 @@
         <dashboard-header>
             <h1 class="text-lg leading-5 md:text-xl lg:text-2xl align-middle generalsans-medium">Account Settings</h1>
         </dashboard-header>
-        <div class="flex flex-col items-start justify-start w-1/2">
+        <div class="flex flex-col items-start justify-start md:w-1/2">
             <label class="generalsans-semibold" for="">E-mail</label>
             <div class="flex flex-col w-full items-start justify-start">
                 <text-input class="w-full" placeholder="bouchenna.yanis@gmail.com"></text-input>
-                <app-button class="mt-4 w-1/3">update e-mail <fa class="ml-1" icon="pen"/></app-button>
+                <app-button class="mt-4 md:w-1/3">update e-mail <fa class="ml-1" icon="pen"/></app-button>
             </div>
             <label class="generalsans-semibold mt-10" for="">Password</label>
             <div class="flex flex-col w-full items-start justify-start">
                 <text-input class="w-full" type="password" placeholder="Old Password"></text-input>
                 <text-input class="w-full" type="password" placeholder="New Password"></text-input>
-                <app-button class="mt-4 w-1/3">update password<fa class="ml-1" icon="pen"/></app-button>
+                <app-button class="mt-4 md:w-1/3">update password<fa class="ml-1" icon="pen"/></app-button>
             </div>
         </div>
     </section>
